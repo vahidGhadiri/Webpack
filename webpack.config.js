6b1491292fd56610c347080193a5bb12e5e3a703
vahid ghadiri <62473529+vahidGhadiri@users.noreply.github.com>
2021-10-14 11:26:53 +0330

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const PATH = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 
 const webpackConfig = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
         path: PATH.resolve(__dirname, 'build'),
-        filename: "bundle.js",
+        filename: "[name].js",
+        publicPath: "/ "
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [{
+            use: 'babel-loader',
+            test: /\.js$/
+        }, {
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            test: /\.css$/
+        }, {
+            use: "file-loader",
+            test: /\.(png|jpeg|jpg|gif)$/
+        }]
     }
 }
 
